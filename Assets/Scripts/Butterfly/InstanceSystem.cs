9788022c9e3042f02413598ba5ae1868b2b046a3
danyow <i.zuucol@gmail.com>
2022-08-25 07:34:15 +0800

    [6e1d43ce98b5b50f921abb0650275b5df9723c3f]

--- Assets/Scripts/Butterfly/InstanceSystem.cs
+++ Assets/Scripts/Butterfly/InstanceSystem.cs
@@ -3,9 +3,12 @@
 #if DEBUG_DIAGNOSTICS
 using System.Diagnostics;
 #endif
+
 using System.Collections.Generic;
 using Butterfly.Component;
 using Butterfly.Utility;
+using Unity.Jobs;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
@@ -59,13 +62,19 @@ namespace Butterfly
 
         protected override void OnUpdate()
         {
+            // 在这个系统中有三个级别的循环：
+            // 
+            // 循环 1：通过唯一实例设置数组。我们会得到共享相同实例设置的实体数组。
+            // 循环 2：通过循环 1 中的实体数组。
+            // 循环 3：通过给定的模板网格中的顶点数组通过实例设置。
+
 #if DEBUG_DIAGNOSTICS
             var stopwatch = new Stopwatch();
             stopwatch.Reset();
             stopwatch.Start();
 #endif
 
-            // 枚举所有实例数据条目。
+            // 循环 1：迭代唯一的实例数据条目。
             EntityManager.GetAllUniqueSharedComponentData(_instanceDataList);
 
             foreach(var instanceData in _instanceDataList)
@@ -90,13 +99,15 @@ namespace Butterfly
                 var vertices = instanceData.templateMesh.vertices;
                 var indices = instanceData.templateMesh.triangles;
 
-                // 实例化蝴蝶以及生成器实体。
+                // 循环 2：迭代实例实体。
                 foreach(var instanceEntity in instanceEntities)
                 {
                     // 检索源数据。
                     var ltw = EntityManager.GetComponentData<LocalToWorld>(instanceEntity);
                     var scale = EntityManager.GetComponentData<NonUniformScale>(instanceEntity);
                     var matrix = float4x4.TRS(ltw.Position, ltw.Rotation, scale.Value);
+
+                    // 循环 3：迭代模板网格中的顶点。
                     CreateEntitiesOverMesh(
                         matrix,
                         EntityManager.GetSharedComponentData<RenderSettings>(instanceEntity),
@@ -113,24 +124,129 @@ namespace Butterfly
 #if DEBUG_DIAGNOSTICS
             stopwatch.Stop();
             var time = 1000f * stopwatch.ElapsedTicks / Stopwatch.Frequency;
-            UnityEngine.Debug.Log($"Instantiation: {time}ms");
+            UnityEngine.Debug.Log($"Instantiation time: {time}ms");
 #endif
         }
 
 #endregion
 
-#region 内部方法
+#region Jobified 初始化器
+
+        // 我们使用并行作业来计算初始数据实例化实体中的组件。
+        // 这样做的主要动机是用 Burst 优化向量数学运算
+        // 我们不期望并行性会大大提高性能。
+        [BurstCompatible]
+        private unsafe struct InitDataJob: IJobParallelFor
+        {
+            [ReadOnly]
+            [NativeDisableUnsafePtrRestriction]
+            public void* vertices;
+
+            [ReadOnly]
+            [NativeDisableUnsafePtrRestriction]
+            public void* indices;
+
+            [ReadOnly]
+            public float4x4 ltw;
+
+            public NativeArray<Triangle> triangles;
+            public NativeArray<Particle> particles;
+            public NativeArray<Translation> translations;
+
+            public void Execute(int index)
+            {
+                var i1 = UnsafeUtility.ReadArrayElement<int>(indices, index * 3);
+                var i2 = UnsafeUtility.ReadArrayElement<int>(indices, index * 3 + 1);
+                var i3 = UnsafeUtility.ReadArrayElement<int>(indices, index * 3 + 2);
+
+                var v1 = UnsafeUtility.ReadArrayElement<float3>(vertices, i1);
+                var v2 = UnsafeUtility.ReadArrayElement<float3>(vertices, i2);
+                var v3 = UnsafeUtility.ReadArrayElement<float3>(vertices, i3);
+
+                v1 = math.mul(ltw, new float4(v1, 1)).xyz;
+                v2 = math.mul(ltw, new float4(v2, 1)).xyz;
+                v3 = math.mul(ltw, new float4(v3, 1)).xyz;
+
+                var vc = (v1 + v2 + v3) / 3;
+                triangles[index] = new Triangle { vertex1 = v1 - vc, vertex2 = v2 - vc, vertex3 = v3 - vc, };
+
+                translations[index] = new Translation { Value = vc, };
+
+                particles[index] = new Particle { random = Random.Value01((uint)index), };
+            }
+        }
 
         /// <summary>
         /// 实例化
         /// </summary>
-        private void CreateEntitiesOverMesh(
+        private unsafe void CreateEntitiesOverMesh(
             float4x4 matrix,
             RenderSettings renderSettings,
-            IReadOnlyList<Vector3> vertices,
-            IReadOnlyList<int> indices
+            Vector3[] vertices,
+            int[] indices
         )
         {
+            // // 为这个查询创建一个渲染器。
+            // var renderer = new Renderer
+            // {
+            //     settings = renderSettings,
+            //     workMesh = new Mesh(),
+            //     vertices = new Vector3[Renderer.MaxVertices],
+            //     normals = new Vector3[Renderer.MaxVertices],
+            //     counter = new NativeCounter(Allocator.Persistent),
+            // };
+            //
+            // _toBeDisposed.Add(renderer);
+            //
+            // // 创建模板实体。
+            // var template = EntityManager.CreateEntity(_archetype);
+            // EntityManager.SetSharedComponentData(template, renderer);
+            //
+            // // 克隆模板实体。
+            // var clones = new NativeArray<Entity>(indices.Length / 3, Allocator.Temp);
+            // EntityManager.Instantiate(template, clones);
+            //
+            // // 设置初始数据。
+            // for(var i = 0; i < clones.Length; i++)
+            // {
+            //     var v1 = math.mul(matrix, new float4(vertices[indices[i * 3 + 0]], 1)).xyz;
+            //     var v2 = math.mul(matrix, new float4(vertices[indices[i * 3 + 1]], 1)).xyz;
+            //     var v3 = math.mul(matrix, new float4(vertices[indices[i * 3 + 2]], 1)).xyz;
+            //     var vc = (v1 + v2 + v3) / 3;
+            //
+            //     var entity = clones[i];
+            //
+            //     EntityManager.SetComponentData(
+            //         entity,
+            //         new Triangle { vertex1 = v1 - vc, vertex2 = v2 - vc, vertex3 = v3 - vc, }
+            //     );
+            //
+            //     EntityManager.SetComponentData(entity, new Translation { Value = vc, });
+            //
+            //     EntityManager.SetComponentData(entity, new Particle { random = Random.Value01((uint)i), });
+            // }
+            //
+            // // 销毁模板对象。
+            // EntityManager.DestroyEntity(template);
+            // clones.Dispose();
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            var entityCount = indices.Length / 3;
+
+            // 使用并行作业计算初始数据。
+            var job = new InitDataJob
+            {
+                vertices = UnsafeUtility.AddressOf(ref vertices[0]),
+                indices = UnsafeUtility.AddressOf(ref indices[0]),
+                ltw = matrix,
+                triangles = new NativeArray<Triangle>(entityCount, Allocator.TempJob, NativeArrayOptions.UninitializedMemory),
+                translations = new NativeArray<Translation>(entityCount, Allocator.TempJob, NativeArrayOptions.UninitializedMemory),
+                particles = new NativeArray<Particle>(entityCount, Allocator.TempJob, NativeArrayOptions.UninitializedMemory),
+            };
+
+            var jobHandle = job.Schedule(entityCount, 32);
+
             // 为这个查询创建一个渲染器。
             var renderer = new Renderer
             {
@@ -144,36 +260,29 @@ namespace Butterfly
             _toBeDisposed.Add(renderer);
 
             // 创建模板实体。
-            var template = EntityManager.CreateEntity(_archetype);
-            EntityManager.SetSharedComponentData(template, renderer);
+            var defaultEntity = EntityManager.CreateEntity(_archetype);
+            EntityManager.SetSharedComponentData(defaultEntity, renderer);
 
             // 克隆模板实体。
-            var clones = new NativeArray<Entity>(indices.Count / 3, Allocator.Temp);
-            EntityManager.Instantiate(template, clones);
+            var entities = new NativeArray<Entity>(entityCount, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
+            EntityManager.Instantiate(defaultEntity, entities);
 
-            // 设置初始数据。
-            for(var i = 0; i < clones.Length; i++)
+            jobHandle.Complete();
+
+            for(var i = 0; i < entityCount; i++)
             {
-                var v1 = math.mul(matrix, new float4(vertices[indices[i * 3 + 0]], 1)).xyz;
-                var v2 = math.mul(matrix, new float4(vertices[indices[i * 3 + 1]], 1)).xyz;
-                var v3 = math.mul(matrix, new float4(vertices[indices[i * 3 + 2]], 1)).xyz;
-                var vc = (v1 + v2 + v3) / 3;
+                var entity = entities[i];
 
-                var entity = clones[i];
-
-                EntityManager.SetComponentData(
-                    entity,
-                    new Triangle { vertex1 = v1 - vc, vertex2 = v2 - vc, vertex3 = v3 - vc, }
-                );
-
-                EntityManager.SetComponentData(entity, new Translation { Value = vc, });
-
-                EntityManager.SetComponentData(entity, new Particle { random = Random.Value01((uint)i), });
+                EntityManager.SetComponentData(entity, job.triangles[i]);
+                EntityManager.SetComponentData(entity, job.particles[i]);
+                EntityManager.SetComponentData(entity, job.translations[i]);
             }
 
-            // 销毁模板对象。
-            EntityManager.DestroyEntity(template);
-            clones.Dispose();
+            EntityManager.DestroyEntity(defaultEntity);
+            entities.Dispose();
+            job.triangles.Dispose();
+            job.particles.Dispose();
+            job.translations.Dispose();
         }
 
 #endregion
